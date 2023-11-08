e695f9c1b5f789aca1781cb4577d2b303448365f
Ilya <IlyaMails@yandex.ru>
2023-11-08 03:19:08 +0300

    v0.1.1

--- algorithm.go
+++ algorithm.go
@@ -1,20 +1,8 @@
-package main
+package algorithm
 
 import (
-	"fmt"
-
 	"golang.org/x/exp/constraints"
 )
-
-func main() {
-	S1 := []int{1, 2, 3, 4, 5}
-	S2 := []string{"c", "b", "a"}
-	S3 := []float32{4.0, 2.0, 7.0}
-
-	fmt.Println(Sort(S1))
-	fmt.Println(Sort(S2))
-	fmt.Println(Sort(S3))
-}
 
 func Sort[T constraints.Ordered](s []T) ([]T, error) {
 
